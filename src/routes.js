f5d4cab5df68b29714f6a93fcaac2b510f21177f
fcristianacc <fcristianacc@gmail.com>
2020-09-17 20:23:12 +0100

    Configuration of the required startup files

--- src/routes.js
+++ src/routes.js
@@ -3,18 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { Route, Link, BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import Home from "./components/home/home.js";
 import Login from "./components/login/login.js";
 
 const routing = (
   <Router>
     <div>
-      <Route path="/" component={App} />
-      <Route path="/login" component={Login} />
       <ul>
         <li>
           <Link to="/login">Login</Link>
         </li>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
       </ul>
+      <Route path="/login" component={Login} />
+      <Route path="/home" component={Home} />
     </div>
   </Router>
 );
